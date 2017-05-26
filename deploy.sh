a300bffcfbefa43f5041b0aafa7f6ebcdebc2d02
scrpgil <scraplbuild@gmail.com>
2017-05-26 18:47:40 +0900

    [add]README追加＆deploy.sh変更

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
+ionic build browser --prod
 cd platforms/browser/www
 git init
+git add . 
+git commit -m "auto deploy" 
+git branch gh-pages 
+git checkout gh-pages 
 git remote add origin https://github.com/scrpgil/tapWar.git
-git branch gh-pages && git checkout gh-pages && git add . &&  git commit -m "auto update" &&  git push -f origin gh-pages
+git push -f origin gh-pages
